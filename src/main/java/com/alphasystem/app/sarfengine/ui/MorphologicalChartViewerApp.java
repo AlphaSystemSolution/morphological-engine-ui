ac50d6ccec17c460617f83e5ddcc0c1de846d5bf
sfali23 <f.syed.ali@gmail.com>
2017-01-05 14:02:38 -0500

    using MorphologicalEngine

--- src/main/java/com/alphasystem/app/sarfengine/ui/MorphologicalChartViewerApp.java
+++ src/main/java/com/alphasystem/app/sarfengine/ui/MorphologicalChartViewerApp.java
@@ -1,12 +1,12 @@
 package com.alphasystem.app.sarfengine.ui;
 
-import com.alphasystem.app.morphologicalengine.conjugation.builder.ConjugationBuilder;
-import com.alphasystem.app.morphologicalengine.conjugation.builder.ConjugationRoots;
-import com.alphasystem.app.morphologicalengine.conjugation.model.NounRootBase;
-import com.alphasystem.app.morphologicalengine.guice.GuiceSupport;
+import com.alphasystem.app.morphologicalengine.conjugation.model.MorphologicalChart;
+import com.alphasystem.app.morphologicalengine.docx.MorphologicalChartEngine;
 import com.alphasystem.app.morphologicalengine.ui.MorphologicalChartControl;
-import com.alphasystem.app.morphologicalengine.util.VerbalNounFactory;
 import com.alphasystem.arabic.model.ArabicLetterType;
+import com.alphasystem.morphologicalanalysis.morphology.model.ConjugationData;
+import com.alphasystem.morphologicalanalysis.morphology.model.ConjugationTemplate;
+import com.alphasystem.morphologicalanalysis.morphology.model.RootLetters;
 import com.alphasystem.morphologicalanalysis.morphology.model.support.VerbalNoun;
 import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
@@ -15,7 +15,6 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
-import static com.alphasystem.app.morphologicalengine.conjugation.builder.ConjugationHelper.getConjugationRoots;
 import static com.alphasystem.arabic.model.NamedTemplate.FORM_I_CATEGORY_I_GROUP_A_TEMPLATE;
 import static javafx.scene.control.ScrollPane.ScrollBarPolicy.AS_NEEDED;
 
@@ -41,16 +40,19 @@ public class MorphologicalChartViewerApp extends Application {
         primaryStage.setWidth(bounds.getWidth() / 4);
         primaryStage.setHeight(bounds.getHeight() / 4);
 
-        ConjugationBuilder conjugationBuilder = GuiceSupport.getInstance().getConjugationBuilder();
+        ConjugationTemplate conjugationTemplate = new ConjugationTemplate();
+        ConjugationData conjugationData = new ConjugationData();
+        conjugationData.setTemplate(FORM_I_CATEGORY_I_GROUP_A_TEMPLATE);
+        conjugationData.setTranslation("To Mercy");
+        conjugationData.setVerbalNouns(VerbalNoun.VERBAL_NOUN_V1, VerbalNoun.VERBAL_NOUN_V27);
+        conjugationData.setRootLetters(new RootLetters(ArabicLetterType.RA, ArabicLetterType.HHA, ArabicLetterType.MEEM));
+        conjugationTemplate.getData().add(conjugationData);
 
-        final NounRootBase[] verbalNouns = {VerbalNounFactory.getByVerbalNoun(VerbalNoun.VERBAL_NOUN_V1),
-                VerbalNounFactory.getByVerbalNoun(VerbalNoun.VERBAL_NOUN_V27)};
-        final ConjugationRoots conjugationRoots = getConjugationRoots(FORM_I_CATEGORY_I_GROUP_A_TEMPLATE, "To Mercy",
-                verbalNouns, null);
+        MorphologicalChartEngine engine = new MorphologicalChartEngine(conjugationTemplate);
+        final MorphologicalChart morphologicalChart = engine.createMorphologicalCharts().get(0);
 
         MorphologicalChartControl morphologicalChartControl = new MorphologicalChartControl();
-        morphologicalChartControl.setMorphologicalChart(conjugationBuilder.doConjugation(conjugationRoots,
-                ArabicLetterType.RA, ArabicLetterType.HHA, ArabicLetterType.MEEM, null));
+        morphologicalChartControl.setMorphologicalChart(morphologicalChart);
 
         ScrollPane scrollPane = new ScrollPane();
         scrollPane.setContent(morphologicalChartControl);
@@ -60,7 +62,6 @@ public class MorphologicalChartViewerApp extends Application {
         primaryStage.setMaximized(true);
         primaryStage.setScene(scene);
         primaryStage.show();
-
     }
 
 }
