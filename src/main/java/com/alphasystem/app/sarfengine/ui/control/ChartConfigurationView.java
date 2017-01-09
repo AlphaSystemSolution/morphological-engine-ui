2ec3c832652d2a7f0ea580c24a072e437bd67d3a
sfali23 <f.syed.ali@gmail.com>
2017-01-08 22:53:57 -0500

    Adding configuration for Font settings

--- src/main/java/com/alphasystem/app/sarfengine/ui/control/ChartConfigurationView.java
+++ src/main/java/com/alphasystem/app/sarfengine/ui/control/ChartConfigurationView.java
@@ -1,21 +1,23 @@
 package com.alphasystem.app.sarfengine.ui.control;
 
+import com.alphasystem.app.morphologicalengine.ui.util.MorphologicalEnginePreferences;
 import com.alphasystem.app.sarfengine.ui.control.skin.ChartConfigurationSkin;
 import com.alphasystem.morphologicalanalysis.morphology.model.ChartConfiguration;
 import com.alphasystem.morphologicalanalysis.morphology.model.support.SortDirection;
 import com.alphasystem.morphologicalanalysis.morphology.model.support.SortDirective;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.property.SimpleObjectProperty;
+import com.alphasystem.util.GenericPreferences;
+import javafx.beans.property.*;
 import javafx.scene.control.Control;
 import javafx.scene.control.Skin;
+
+import static com.alphasystem.util.AppUtil.getResource;
 
 /**
  * @author sali
  */
 public class ChartConfigurationView extends Control {
 
+    private final MorphologicalEnginePreferences preferences = (MorphologicalEnginePreferences) GenericPreferences.getInstance();
     private final ObjectProperty<ChartConfiguration> chartConfiguration = new SimpleObjectProperty<>();
     private final BooleanProperty omitToc = new SimpleBooleanProperty();
     private final BooleanProperty omitAbbreviatedConjugation = new SimpleBooleanProperty();
@@ -25,6 +27,12 @@ public class ChartConfigurationView extends Control {
     private final BooleanProperty omitSarfTermCaption = new SimpleBooleanProperty();
     private final ObjectProperty<SortDirective> sortDirective = new SimpleObjectProperty<>();
     private final ObjectProperty<SortDirection> sortDirection = new SimpleObjectProperty<>();
+    private final StringProperty arabicFontFamily = new SimpleStringProperty(this, "arabicFontFamily");
+    private final StringProperty translationFontFamily = new SimpleStringProperty(this, "translationFontFamily");
+    private final LongProperty arabicFontSize = new SimpleLongProperty(this, "arabicFontSize");
+    private final LongProperty arabicUiFontSize = new SimpleLongProperty(this, "arabicUiFontSize");
+    private final LongProperty translationFontSize = new SimpleLongProperty(this, "translationFontSize");
+    private final LongProperty translationUiFontSize = new SimpleLongProperty(this, "translationUiFontSize");
 
     public ChartConfigurationView() {
         chartConfigurationProperty().addListener((o, ov, nv) -> {
@@ -36,36 +44,44 @@ public class ChartConfigurationView extends Control {
             setOmitToc(nv.isOmitToc());
             setSortDirection(nv.getSortDirection());
             setSortDirective(nv.getSortDirective());
+            setArabicFontFamily(nv.getArabicFontFamily());
+            setTranslationFontFamily(nv.getTranslationFontFamily());
+            setArabicFontSize(nv.getArabicFontSize());
+            setArabicUiFontSize(preferences.getArabicFontSize());
+            setTranslationFontSize(nv.getTranslationFontSize());
+            setTranslationUiFontSize(preferences.getEnglishFontSize());
         });
 
-        omitAbbreviatedConjugationProperty().addListener((o, ov, nv) -> {
-            getChartConfiguration().setOmitAbbreviatedConjugation(nv);
+        omitAbbreviatedConjugationProperty().addListener((o, ov, nv) -> getChartConfiguration().setOmitAbbreviatedConjugation(nv));
+        omitDetailedConjugationProperty().addListener((o, ov, nv) -> getChartConfiguration().setOmitDetailedConjugation(nv));
+        omitHeaderProperty().addListener((o, ov, nv) -> getChartConfiguration().setOmitHeader(nv));
+        omitSarfTermCaptionProperty().addListener((o, ov, nv) -> getChartConfiguration().setOmitSarfTermCaption(nv));
+        omitTitleProperty().addListener((o, ov, nv) -> getChartConfiguration().setOmitTitle(nv));
+        omitTocProperty().addListener((o, ov, nv) -> getChartConfiguration().setOmitToc(nv));
+        sortDirectionProperty().addListener((o, ov, nv) -> getChartConfiguration().setSortDirection(nv));
+        sortDirectiveProperty().addListener((o, ov, nv) -> getChartConfiguration().setSortDirective(nv));
+        arabicFontFamilyProperty().addListener((o, ov, nv) -> {
+            getChartConfiguration().setArabicFontFamily(nv);
+            preferences.setArabicFontName(nv);
         });
-        omitDetailedConjugationProperty().addListener((o, ov, nv) -> {
-            getChartConfiguration().setOmitDetailedConjugation(nv);
+        translationFontFamilyProperty().addListener((o, ov, nv) -> {
+            getChartConfiguration().setTranslationFontFamily(nv);
+            preferences.setEnglishFontName(nv);
         });
-        omitHeaderProperty().addListener((o, ov, nv) -> {
-            getChartConfiguration().setOmitHeader(nv);
-        });
-        omitSarfTermCaptionProperty().addListener((o, ov, nv) -> {
-            getChartConfiguration().setOmitSarfTermCaption(nv);
-        });
-        omitTitleProperty().addListener((o, ov, nv) -> {
-            getChartConfiguration().setOmitTitle(nv);
-        });
-        omitTocProperty().addListener((o, ov, nv) -> {
-            getChartConfiguration().setOmitToc(nv);
-        });
-        sortDirectionProperty().addListener((o, ov, nv) -> {
-            getChartConfiguration().setSortDirection(nv);
-        });
-        sortDirectiveProperty().addListener((o, ov, nv) -> {
-            getChartConfiguration().setSortDirective(nv);
-        });
+        arabicFontSizeProperty().addListener((o, ov, nv) -> getChartConfiguration().setArabicFontSize((Long) nv));
+        translationFontSizeProperty().addListener((o, ov, nv) -> getChartConfiguration().setTranslationFontSize((Long) nv));
+        arabicUiFontSizeProperty().addListener((o, ov, nv) -> preferences.setArabicFontSize((Long) nv));
+        translationUiFontSizeProperty().addListener((o, ov, nv) -> preferences.setEnglishFontSize((Long) nv));
 
         setChartConfiguration(null);
-        setMinWidth(400);
+        setMinWidth(600);
     }
+
+    @Override
+    public String getUserAgentStylesheet() {
+        return getResource("arabic-ui-support.css").toExternalForm();
+    }
+
 
     @Override
     protected Skin<?> createDefaultSkin() {
@@ -146,5 +162,77 @@ public class ChartConfigurationView extends Control {
 
     public final void setSortDirection(SortDirection sortDirection) {
         this.sortDirection.set(sortDirection);
+    }
+
+    public final String getArabicFontFamily() {
+        return arabicFontFamily.get();
+    }
+
+    public final StringProperty arabicFontFamilyProperty() {
+        return arabicFontFamily;
+    }
+
+    public final void setArabicFontFamily(String arabicFontFamily) {
+        this.arabicFontFamily.set(arabicFontFamily);
+    }
+
+    public final String getTranslationFontFamily() {
+        return translationFontFamily.get();
+    }
+
+    public final StringProperty translationFontFamilyProperty() {
+        return translationFontFamily;
+    }
+
+    public final void setTranslationFontFamily(String translationFontFamily) {
+        this.translationFontFamily.set(translationFontFamily);
+    }
+
+    public final long getArabicFontSize() {
+        return arabicFontSize.get();
+    }
+
+    public final LongProperty arabicFontSizeProperty() {
+        return arabicFontSize;
+    }
+
+    public final void setArabicFontSize(long arabicFontSize) {
+        this.arabicFontSize.set(arabicFontSize);
+    }
+
+    public final long getArabicUiFontSize() {
+        return arabicUiFontSize.get();
+    }
+
+    public final LongProperty arabicUiFontSizeProperty() {
+        return arabicUiFontSize;
+    }
+
+    public final void setArabicUiFontSize(long arabicUiFontSize) {
+        this.arabicUiFontSize.set(arabicUiFontSize);
+    }
+
+    public final long getTranslationFontSize() {
+        return translationFontSize.get();
+    }
+
+    public final LongProperty translationFontSizeProperty() {
+        return translationFontSize;
+    }
+
+    public final void setTranslationFontSize(long translationFontSize) {
+        this.translationFontSize.set(translationFontSize);
+    }
+
+    public final long getTranslationUiFontSize() {
+        return translationUiFontSize.get();
+    }
+
+    public final LongProperty translationUiFontSizeProperty() {
+        return translationUiFontSize;
+    }
+
+    public final void setTranslationUiFontSize(long translationUiFontSize) {
+        this.translationUiFontSize.set(translationUiFontSize);
     }
 }
--- src/main/java/com/alphasystem/app/sarfengine/ui/control/skin/ChartConfigurationSkin.java
+++ src/main/java/com/alphasystem/app/sarfengine/ui/control/skin/ChartConfigurationSkin.java
@@ -1,15 +1,14 @@
 package com.alphasystem.app.sarfengine.ui.control.skin;
 
 import com.alphasystem.app.sarfengine.ui.control.ChartConfigurationView;
+import com.alphasystem.fx.ui.util.FontSizeStringConverter;
 import com.alphasystem.morphologicalanalysis.morphology.model.support.SortDirection;
 import com.alphasystem.morphologicalanalysis.morphology.model.support.SortDirective;
 import javafx.geometry.Insets;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.SkinBase;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
 
 import static javafx.collections.FXCollections.observableArrayList;
 
@@ -17,6 +16,8 @@ import static javafx.collections.FXCollections.observableArrayList;
  * @author sali
  */
 public class ChartConfigurationSkin extends SkinBase<ChartConfigurationView> {
+
+    private static final Long[] SIZE_ARRAY = new Long[]{8L, 9L, 10L, 11L, 12L, 14L, 16L, 18L, 20L, 22L, 24L, 26L, 28L, 30L, 36L, 48L, 72L};
 
     /**
      * @param control chart configuration control
@@ -29,9 +30,9 @@ public class ChartConfigurationSkin extends SkinBase<ChartConfigurationView> {
         getChildren().add(mainPane);
     }
 
-    private GridPane initializePane() {
+    private BorderPane initializeConfigurationPane() {
         GridPane gridPane = new GridPane();
-        gridPane.setPadding(new Insets(10));
+        gridPane.setPadding(new Insets(30, 30, 30, 30));
         gridPane.setVgap(10);
         gridPane.setHgap(10);
 
@@ -83,6 +84,117 @@ public class ChartConfigurationSkin extends SkinBase<ChartConfigurationView> {
         gridPane.add(sortDirectiveComboBox, 0, row);
         gridPane.add(sortDirectionComboBox, 1, row);
 
-        return gridPane;
+        gridPane.getStyleClass().add("view");
+        BorderPane borderPane = new BorderPane();
+        borderPane.setPadding(new Insets(20, 20, 20, 20));
+        borderPane.setCenter(gridPane);
+
+        return borderPane;
+    }
+
+    private BorderPane initializeFontPane() {
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(30, 30, 30, 30));
+        gridPane.setVgap(10);
+        gridPane.setHgap(10);
+
+        ChartConfigurationView view = getSkinnable();
+
+        int row = 0;
+        Label label = new Label("Arabic Font Family:");
+        gridPane.add(label, 0, row);
+        ComboBox<String> arabicFontFamilyComboBox = new ComboBox<>();
+        arabicFontFamilyComboBox.getItems().addAll(Font.getFamilies());
+        arabicFontFamilyComboBox.getSelectionModel().selectFirst();
+        label.setLabelFor(arabicFontFamilyComboBox);
+        arabicFontFamilyComboBox.valueProperty().bindBidirectional(view.arabicFontFamilyProperty());
+
+        label = new Label("Translation Font Family");
+        gridPane.add(label, 1, row);
+        ComboBox<String> translationFontFamilyComboBox = new ComboBox<>();
+        translationFontFamilyComboBox.getItems().addAll(Font.getFamilies());
+        translationFontFamilyComboBox.getSelectionModel().selectFirst();
+        label.setLabelFor(translationFontFamilyComboBox);
+        translationFontFamilyComboBox.valueProperty().bindBidirectional(view.translationFontFamilyProperty());
+
+        row++;
+        gridPane.add(arabicFontFamilyComboBox, 0, row);
+        gridPane.add(translationFontFamilyComboBox, 1, row);
+
+        row++;
+        label = new Label("UI Arabic Font Size:");
+        gridPane.add(label, 0, row);
+        ComboBox<Long> uiArabicFontSizeComboBox = new ComboBox<>();
+        uiArabicFontSizeComboBox.setEditable(true);
+        uiArabicFontSizeComboBox.getItems().addAll(SIZE_ARRAY);
+        uiArabicFontSizeComboBox.setConverter(new FontSizeStringConverter());
+        uiArabicFontSizeComboBox.getSelectionModel().select(view.getArabicUiFontSize());
+        label.setLabelFor(uiArabicFontSizeComboBox);
+        uiArabicFontSizeComboBox.valueProperty().addListener((observable, oldValue, newValue) -> view.setArabicUiFontSize(newValue));
+        view.arabicUiFontSizeProperty().addListener((observable, oldValue, newValue) -> uiArabicFontSizeComboBox.setValue((Long) newValue));
+
+        label = new Label("UI Translation Font Size:");
+        gridPane.add(label, 1, row);
+        ComboBox<Long> uiTranslationFontSizeComboBox = new ComboBox<>();
+        uiTranslationFontSizeComboBox.setEditable(true);
+        uiTranslationFontSizeComboBox.getItems().addAll(SIZE_ARRAY);
+        uiTranslationFontSizeComboBox.setConverter(new FontSizeStringConverter());
+        uiTranslationFontSizeComboBox.getSelectionModel().select(view.getTranslationUiFontSize());
+        label.setLabelFor(uiTranslationFontSizeComboBox);
+        uiTranslationFontSizeComboBox.valueProperty().addListener((observable, oldValue, newValue) -> view.setTranslationUiFontSize(newValue));
+        view.translationUiFontSizeProperty().addListener((observable, oldValue, newValue) -> uiTranslationFontSizeComboBox.setValue((Long) newValue));
+
+        row++;
+        gridPane.add(uiArabicFontSizeComboBox, 0, row);
+        gridPane.add(uiTranslationFontSizeComboBox, 1, row);
+
+        row++;
+        label = new Label("Word Arabic Font Size:");
+        gridPane.add(label, 0, row);
+        ComboBox<Long> wordArabicFontSizeComboBox = new ComboBox<>();
+        wordArabicFontSizeComboBox.setEditable(true);
+        wordArabicFontSizeComboBox.getItems().addAll(SIZE_ARRAY);
+        wordArabicFontSizeComboBox.setConverter(new FontSizeStringConverter());
+        wordArabicFontSizeComboBox.getSelectionModel().select(view.getArabicFontSize());
+        label.setLabelFor(wordArabicFontSizeComboBox);
+        wordArabicFontSizeComboBox.valueProperty().addListener((observable, oldValue, newValue) -> view.setArabicFontSize(newValue));
+        view.translationFontSizeProperty().addListener((observable, oldValue, newValue) -> wordArabicFontSizeComboBox.setValue((Long) newValue));
+
+        label = new Label("Word Translation Font Size:");
+        gridPane.add(label, 1, row);
+        ComboBox<Long> wordTranslationFontSizeComboBox = new ComboBox<>();
+        wordTranslationFontSizeComboBox.setEditable(true);
+        wordTranslationFontSizeComboBox.getItems().addAll(SIZE_ARRAY);
+        wordTranslationFontSizeComboBox.setConverter(new FontSizeStringConverter());
+        wordTranslationFontSizeComboBox.getSelectionModel().select(view.getTranslationFontSize());
+        label.setLabelFor(wordTranslationFontSizeComboBox);
+        wordTranslationFontSizeComboBox.valueProperty().addListener((observable, oldValue, newValue) -> view.setTranslationFontSize(newValue));
+        view.translationFontSizeProperty().addListener((observable, oldValue, newValue) -> wordTranslationFontSizeComboBox.setValue((Long) newValue));
+
+        row++;
+        gridPane.add(wordArabicFontSizeComboBox, 0, row);
+        gridPane.add(wordTranslationFontSizeComboBox, 1, row);
+
+        gridPane.getStyleClass().add("view");
+        BorderPane borderPane = new BorderPane();
+        borderPane.setPadding(new Insets(20, 20, 20, 20));
+        borderPane.setCenter(gridPane);
+
+        return borderPane;
+    }
+
+    private BorderPane initializePane() {
+        BorderPane borderPane = new BorderPane();
+
+        TabPane tabPane = new TabPane();
+        tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
+        tabPane.getSelectionModel().selectFirst();
+
+        tabPane.getTabs().add(new Tab("Chart Configuration", initializeConfigurationPane()));
+        tabPane.getTabs().add(new Tab("Font Configuration", initializeFontPane()));
+
+        borderPane.setCenter(tabPane);
+
+        return borderPane;
     }
 }
